2b8256f2cd1df2731bcd450eb88b62529dbf780c
David Vielhuber <david@vielhuber.de>
2025-05-23 11:23:56 +0200

    - Add a `testTimeout` of 60,000 milliseconds. - Set `testEnvironmentOptions` with a specific `url`. - Maintain existing `automock` and `preset` configuration.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     automock: false,
     preset: 'jest-puppeteer',
-    testMatch: ['**/_tests/_js/**/*.js?(x)']
+    testMatch: ['**/_tests/_js/**/*.js?(x)'],
+    testTimeout: 60 * 1000,
+    testEnvironmentOptions: {
+        url: 'https://tld.com/?foo=bar&bar=baz',
+    },
 };

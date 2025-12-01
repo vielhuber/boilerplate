351fa312110b6de53d5826eb9f7c257ad6602770
David Vielhuber <david@vielhuber.de>
2025-12-01 12:01:47 +0100

    Improve rem support.

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-module.exports = {
+export default {
     automock: false,
     preset: 'jest-puppeteer',
     testMatch: ['**/_tests/_js/**/*.js?(x)'],
     testTimeout: 60 * 1000,
     testEnvironmentOptions: {
-        url: 'https://tld.com/?foo=bar&bar=baz',
-    },
+        url: 'https://tld.com/?foo=bar&bar=baz'
+    }
 };

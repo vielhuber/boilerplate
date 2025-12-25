1f5209e80920dc86a86c5a22e134da9078c65d0d
David Vielhuber <david@vielhuber.de>
2025-12-25 11:58:02 +0100

    Improve boilerplate scrollbars.

--- _tests/_js/integration/example.test.js
+++ _tests/_js/integration/example.test.js
@@ -1,10 +1,10 @@
 describe('Test puppeteer', () => {
     beforeAll(async () => {
         await page.goto('https://google.com', { waitUntil: 'networkidle2' });
-    }, 3000);
+    }, 6000);
 
     it('should display "google" text on page', async () => {
         await page.waitForSelector('body');
         await expect(page.title()).resolves.toMatch('Google');
-    }, 3000);
+    }, 6000);
 });

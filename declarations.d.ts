a98b665feda3166a663276539f78890dd1875f89
David Vielhuber <david@vielhuber.de>
2026-04-30 10:18:45 +0200

    Improve declarations.

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,16 +1,35 @@
 declare module '*.css';
 
+declare const jQuery: any;
+declare const hlp: any;
+declare const google: any;
+
+interface Window {
+    [key: string]: any;
+}
 interface Document {
     querySelector<T = any>(selectors: string & {}): T;
     querySelectorAll<T = any>(selectors: string & {}): NodeListOf<T>;
     getElementById<T = any>(elementId: string): T;
+    [key: string]: any;
 }
 interface Element {
     querySelector<T = any>(selectors: string & {}): T;
     querySelectorAll<T = any>(selectors: string & {}): NodeListOf<T>;
+    closest<T = any>(selectors: string & {}): T;
 }
 interface DocumentFragment {
     querySelector<T = any>(selectors: string & {}): T;
     querySelectorAll<T = any>(selectors: string & {}): NodeListOf<T>;
     getElementById<T = any>(elementId: string): T;
+}
+interface EventTarget {
+    closest<T = any>(selectors: string & {}): T;
+}
+interface ParentNode {
+    classList: DOMTokenList;
+    style: CSSStyleDeclaration;
+}
+interface ArrayConstructor {
+    from<T = any>(arrayLike: any): T[];
 }
